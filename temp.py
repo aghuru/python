b4b896258afe95d2d58452e6029810800b8d6709
aghuru <aghuru.sas@gmail.com>
2022-01-23 18:54:46 -0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
-#from datetime import datetime
-#print(datetime.today().strftime('%A, %B %d, %Y %H:%M:%S') +" --> Procesando sorteo del: " + str(fecha_dt) + " <-")
+from datetime import datetime
+
+print(datetime.today().strftime('%A, %B %d, %Y %H:%M:%S') +" --> Procesando sorteo del: " + str(fecha_dt) + " <-")
 
 
 print("corrio 220123")
